3e2697fcc20b13bf28e01889564df6d435264213
Zeek <zeekg@amazon.com>
2023-11-06 11:45:00 -0500

    firstcommit

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,9 +1,19 @@
 import React, { useState} from 'react';
 import axios from 'axios';
 
+
+const axiosInstance = axios.create({
+    baseURL: 'https://2djzi835pk.execute-api.us-east-1.amazonaws.com/dev',  
+    withCredentials: false  // Enable credentials
+  });
+
+
+  
+  
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event) => {
     setInput(event.target.value);
@@ -16,19 +26,33 @@ const Chatbot = () => {
       const newMessages = [...messages, { text: input, user: 'user' }];
       setMessages(newMessages);
       setInput('');
+      setLoading(true);
 
       // Simulate bot response
       try {
-        const response = await axios.post('YOUR_API_ENDPOINT', {
-          message: input
+        const response = await axiosInstance.post('/', {
+          question: input
         });
-        setMessages([...newMessages, { text: response.data.message, user: 'bot' }]);
+        const responseBody = JSON.parse(response.data.body); // response from the lambda function
+        const botResponse = responseBody.message;
+        console.log('Bot Response:', botResponse);
+        const preFormatedResponse = <pre>{botResponse}</pre>
+        setMessages([...newMessages, { text: preFormatedResponse, user: 'bot' }]);
       } catch (error) {
         console.error('Error sending message to the bot:', error);
+      }finally {
+        setLoading(false);
       }
     }
   };
-
+  const clearChat = async () => {
+    try {
+      setMessages([]); // Clear the chat history in the frontend
+      const response = await axiosInstance.post('https://gxvrtikdbl.execute-api.us-east-1.amazonaws.com/dev'); 
+    } catch (error) {
+      console.error('Error clearing chat history:', error);
+    }
+  };
   return (
     <div className="chatbot-container">
       <div className="chatbot-messages">
@@ -38,6 +62,13 @@ const Chatbot = () => {
           </div>
         ))}
       </div>
+      <div className="loading-gif-container">
+        {loading && (
+          <img src="/loading.gif" alt="Loading" 
+          style={{ width: '128px', height: '23px' }}/>
+          
+        )}
+      </div>
       <form onSubmit={handleSendMessage} className="chatbot-input">
         <input
           type="text"
@@ -46,9 +77,13 @@ const Chatbot = () => {
           placeholder="Type a message..."
         />
         <button type="submit">Send</button>
+        <button onClick={clearChat}>Clear Chat</button>
       </form>
     </div>
   );
 };
 
+
 export default Chatbot;
+
+
